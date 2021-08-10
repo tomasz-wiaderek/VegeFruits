f4c9af384ee74d2ff8e5569ec5c1456a6ee7170e
tomasz-wiaderek <t.wiaderek@gmail.com>
2021-08-10 17:25:58 +0200

    App user_management added, models created

--- VF_project/settings.py
+++ VF_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_management',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'VF_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'VF_database',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'Programer2021!',
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
---
+++ user_management/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class UserType(models.Model):
+    type = models.CharField(max_length=8, choices=[('admin', 'admin'),
+                                                   ('producer', 'producer'),
+                                                   ('customer', 'customer')])
+
+    def __str__(self):
+        return f'{self.pk}: {self.type}'
+
+
+class UserAdditionalInfo(models.Model):
+    phone = models.CharField(max_length=12)
+    additional_desc = models.TextField(null=True)
+    delivery_available = models.BooleanField(default=False)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=320)
+    type = models.ForeignKey(UserType, on_delete=models.CASCADE)
+    additional_info = models.ForeignKey(UserAdditionalInfo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.pk}: {self.type} {self.name}'
+
+
+class Voivodship(models.Model):
+    name = models.CharField(max_length=18)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class District(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class UserLocation(models.Model):
+    zip_code = models.CharField(max_length=6)
+    voivodship = models.ForeignKey(Voivodship, on_delete=models.CASCADE)
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Location of user: {self.user}'
