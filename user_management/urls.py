9eaec6f1741c0c269bb8e460d9b072a2ad31b3c1
tomasz-wiaderek <t.wiaderek@gmail.com>
2021-08-16 22:14:49 +0200

    UserCreateView added.

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UserDetailView, UserListView
+from .views import UserDetailView, UserListView, UserCreateView
 
 app_name = 'user'
 urlpatterns = [
     path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
     path('users/', UserListView.as_view(), name='users-list'),
+    path('create/', UserCreateView.as_view(), name='users-create'),
 ]
 
