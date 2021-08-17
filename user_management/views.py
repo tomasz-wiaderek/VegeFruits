8320b164558f846325f3e1b01222aa855ad6af62
tomasz-wiaderek <t.wiaderek@gmail.com>
2021-08-17 12:37:18 +0200

    User location implemented to UserDetailView and template.

--- user_management/views.py
+++ user_management/views.py
@@ -7,10 +7,16 @@ from .forms import UserModelForm, UserLocationModelForm
 # Create your views here.
 
 
+# User class views
+
 class UserDetailView(DetailView):
     template_name = 'user/user_detail.html'
     queryset = User.objects.all()
-#  jak wstawic informacje o lokalizacji ?
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['location'] = UserLocation.objects.get(user=context['object'])
+        return context
 
 
 class UserListView(ListView):
@@ -38,6 +44,8 @@ class UserDeleteView(DeleteView):
         return reverse('user:users-list')
 
 
+# UserLocation class views
+
 class UserLocationCreateView(CreateView):
     template_name = 'user_location/ul_create.html'
     queryset = UserLocation.objects.all()
@@ -48,3 +56,16 @@ class UserLocationUpdateView(UpdateView):
     template_name = 'user_location/ul_create.html'
     queryset = UserLocation.objects.all()
     form_class = UserLocationModelForm
+
+
+class UserLocationDeleteView(DeleteView):
+    template_name = 'user_location/ul_delete.html'
+    queryset = UserLocation.objects.all()
+
+    def get_success_url(self):
+        return reverse('user:users-list')
+
+
+class UserLocationDetailView(DetailView):
+    template_name = 'user_location/ul_detail.html'
+    queryset = UserLocation.objects.all()
