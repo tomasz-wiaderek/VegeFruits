7bf902eaae6d78b1eea9c991ad49ad2d00cb23d7
tomasz-wiaderek <t.wiaderek@gmail.com>
2021-08-26 15:56:30 +0200

    Order_management deleted.

--- order_management/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import DeliveryLocation, Order, OrderLine
-
-# Register your models here.
-
-admin.site.register(Order)
-admin.site.register(OrderLine)
-admin.site.register(DeliveryLocation)
